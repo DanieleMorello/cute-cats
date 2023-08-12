58691644bc4f67da01fd15f6d293fd474ac3d09c
Daniele Morello <85832477+DanieleMorello@users.noreply.github.com>
2023-08-12 02:29:50 +0200

    Add Dark-mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,37 @@ const CAT_URL = "https://api.thecatapi.com/v1/images/search";
 const catsContainer = document.querySelector(".cats");
 
 /**
+ * Seleziona il bottone per il cambio di tema
+ * @type {HTMLButtonElement}
+ */
+const toggleThemeButton = document.getElementById("toggleTheme");
+
+/**
+ * Flag per la modalità dark
+ * @type {boolean}
+ */
+let isDarkMode = true;
+
+/**
+ * Funzione per il cambio di tema
+ */
+function toggleTheme() {
+  // Inverti la modalità del tema
+  isDarkMode = !isDarkMode;
+
+  // Aggiungi/rimuovi la classe "bg-dark-mode" dal body per cambiare lo sfondo
+  document.body.classList.toggle("bg-dark-mode", isDarkMode);
+
+  // Seleziona l'elemento icona all'interno del bottone
+  const icon = toggleThemeButton.querySelector("i");
+
+  // Cambia l'icona in base alla modalità di tema
+  icon.classList.toggle("fa-adjust", isDarkMode);
+  icon.classList.toggle("fa-sun", !isDarkMode);
+  icon.classList.toggle("fa-moon", isDarkMode);
+}
+
+/**
  * Funzione per aggiungere un nuovo gatto
  */
 function addNewCat() {
@@ -23,6 +54,9 @@ function addNewCat() {
       catsContainer.appendChild(img);
     });
 }
+
+// Aggiungi il gestore di eventi per il cambio di tema
+toggleThemeButton.addEventListener("click", toggleTheme);
 
 // Aggiungi il gestore di eventi per l'aggiunta di un nuovo gatto
 document.querySelector(".add_cats").addEventListener("click", addNewCat);
